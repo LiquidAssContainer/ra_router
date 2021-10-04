46b42add5b252f9c36ce06974c74592beafb868b
LiquidAssContainer <LiquidAssContainer@users.noreply.github.com>
2021-10-05 02:56:53 +0500

    Add first task

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Menu } from './components/Menu/Menu';
+import {
+  DriftPage,
+  ForzaPage,
+  HomePage,
+  TimeAttackPage,
+} from './components/Menu/Page';
+
 import './styles/app.css';
 
 export const App = () => {
-  return <></>;
+  return (
+    <Router>
+      <div>
+        <Menu />
+        <div className="page">
+          <Route path="/" exact component={HomePage} />
+          <Route path="/drift" component={DriftPage} />
+          <Route path="/timeattack" component={TimeAttackPage} />
+          <Route path="/forza" component={ForzaPage} />
+        </div>
+      </div>
+    </Router>
+  );
 };
